ed37aab9386f5200c5462faf3eca47973782fa22
jbdeb <jean-baptiste.debize@epitech.eu>
2022-02-24 21:40:12 +0100

    eee

--- main.py
+++ main.py
@@ -5,20 +5,40 @@
 ## main
 ##
 
-from os import kill
-from pprint import pp, pprint
-from re import L
+from pprint import pprint
 import sys
-from tkinter.messagebox import NO
-from unicodedata import name
+from typing import Tuple
 
-from sklearn import pipeline
 contributors  = []
 projects = []
 
 nb_contributors = 0
 nb_projects = 0
 cmds = []
+
+def calcScore(time, score, bb, day):
+    t = bb - day
+
+    if (t < 0):
+        return (t + score - time)
+    t -= time
+    if (t < 0):
+        return (score + t)
+    return score
+
+def getBestProject(projects):
+    bestScore = 0
+    bestTime = 999999
+    idxToSave = 0
+    day = 0
+    for val, project in enumerate(projects):
+        tmp = calcScore(project['time'], project['score'], project['best_before'], day)
+        print(tmp)
+        if bestScore < tmp and bestTime < project['time']:
+            bestScore = tmp
+            bestTime = project['time']
+            idxToSave = val
+    return projects[idxToSave]
 
 def parceSkill(skill: str):
     t = skill.split(" ")
@@ -53,6 +73,7 @@ with open(sys.argv[1]) as f:
             if skills.get(t) == None:
                 skills[t] = []
             tskill["name"] = contributor["name"]
+            tskill["skill"] = t
             skills[t].append(tskill)
     for i in range(nb_project):
         project = parceProject(cmds.pop(0))
@@ -60,27 +81,65 @@ with open(sys.argv[1]) as f:
             project["skills"].append(parceSkill(cmds.pop(0)))
         projects.append(project)
 
-i = 0
-imp_project = []
-for project in projects:
+def checkIfcan(project):
     for skill in project["skills"]:
         name = skill["name"]
         lvl = skill["lvl"]
         skill = skills.get(name)
         if skill == None:
-            imp_project.append(projects.pop(i))
-            i -= 1
-            break
-        b = 0
+            return False
         for t in skill:
             if t["lvl"] >= lvl:
-                b = 1
-                break
-        if b == 0:
-            imp_project.append(projects.pop(i))
-            i -= 1
-            break
-    i += 1
+                return True
+    return False
 
-print(i)
-# pprint(projects)
+def removeCan():
+    i = 0
+    imp_project = []
+    for project in projects:
+        if  checkIfcan(project):
+            i += 1
+        else:
+            imp_project.append(projects.pop(i))
+    return imp_project
+jobOccupi = []
+
+def givePeople(project, date: int):
+    t = []
+    t.append(date + project["time"])
+    for skill in project["skills"]:
+        name = skill["name"]
+        lvl = skill["lvl"]
+        skill = skills.get(name)
+        if skill == None:
+            return False
+        i = 0
+        for people in skill:
+            if people["lvl"] >= lvl:
+                skill.pop(i)
+                t.append(people)
+                break
+            i += 1
+    return t
+
+def pushIn(projectIn : list, date: int):
+    i = 0
+    for p in projectIn:
+        if date >= p[0]:
+            projectIn.pop(i)
+            for people in p[1:]:
+                skills[people["skill"]].append(people)
+        else:
+            i += 1
+
+removeProject = removeCan()
+day = 0
+while (len(project) > 0):
+    project = getBestProject(projects)
+    jobOccupi.append(givePeople(project, day))
+    print(jobOccupi)
+    pushIn(jobOccupi, 8)
+    for r in removeProject:
+        projects.push(r)
+    removeProject = removeCan()
+    day += 1
